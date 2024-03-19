1b9b04b0bd4b4ada68d3ffd269367427a3a3964b
vishakh prakash <vishakhpro2002@gmail.com>
2024-03-19 22:08:44 +0530

    add mongo functionality

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
 import posts from "./routes/posts.mjs";
@@ -7,7 +7,7 @@ import posts from "./routes/posts.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 // Load the /posts routes
