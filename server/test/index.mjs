b8fcc3519179235ea5f52ce7c3eb98f8eb19eb2c
vishakh prakash <vishakhpro2002@gmail.com>
2024-03-21 23:37:20 +0530

    FEAT: - add directory for tests - add command for running tests in server/Dockerfile - add example test in test folder
    CHORES:
    - mocha
    - chai
    - supertest
    - @types for cors abd express
    
    TESTS:
    - docker-composes successfully working

---
+++ server/test/index.mjs
@@ -0,0 +1,13 @@
+import addNumbers from '../index.mjs';
+import { assert } from 'chai';
+
+//describe('add two numbers', () => {
+//    it('adding 2 numbers', () => {
+//        const result = addNumbers(3,4)
+//        assert(result,7)
+//    }
+//    )
+//})
+
+    
+
