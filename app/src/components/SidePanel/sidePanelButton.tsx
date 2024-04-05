125c49e1b627cb793796c8dae3ea7dfe2556b091
vishakh prakash <vishakhpro2002@gmail.com>
2024-04-05 17:10:17 +0530

    FEAT: partial changes to make the dashboard responsive

--- app/src/components/SidePanel/sidePanelButton.tsx
+++ app/src/components/SidePanel/sidePanelButton.tsx
@@ -14,10 +14,15 @@ const SidePanelButton: React.FC<SidePanelButtonContent> = (props: SidePanelButto
   return (
     <div className={`flex items-center mb-6 relative pl-4`}>
       <div className={`w-1.5 h-full bg-blue-500 absolute left-0 top-0 ${isActive ? "" : "hidden"} rounded-r-md`}></div>
-      {props.icon && <span className="mr-3 mt-1">{props.icon}</span>}
+      {props.icon && (
+        <span className={`mr-3 mt-1 ${isActive ? "text-blue-500" : ""}`}>
+          <Link to={props.url}>{props.icon}</Link>
+        </span>
+      )}
+      {/* Render the text only for large screens */}
       <Link
         to={props.url}
-        className={`block text-black hover:text-gray-800 ${isActive ? "text-blue-500" : ""}`}
+        className={`block text-black hover:text-gray-800 ${isActive ? "text-blue-500" : ""} ${props.icon ? "hidden md:block" : ""}`}
       >
         {props.text}
       </Link>
@@ -26,3 +31,4 @@ const SidePanelButton: React.FC<SidePanelButtonContent> = (props: SidePanelButto
 };
 
 export default SidePanelButton;
+
---
+++ app/src/page/Dashboard/DashboardShortCards.tsx
@@ -0,0 +1,47 @@
+import DashboardCard from "./DashboardCard"
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import { IconContext } from "react-icons";
+import { GrMoney } from "react-icons/gr";
+import { IoPeople } from "react-icons/io5";
+import { useEffect, useState } from "react";
+
+const convertToNumber = (value: string) => {
+    return Number(value) || 1;
+};
+
+const DashBoardCardGroup = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken')
+            const response = await fetch('http://localhost:5050/api/posts', {
+                headers: {
+                    'x-access-token': accessToken ? accessToken : ''
+                }
+            }
+            );
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
+        <div className="flex flex-col md:flex-row mt-12 ml-4 w-4/5">
+            <DashboardCard text="Total Sales" icon={<IconContext.Provider value={{ color: "green", size: '24px', className: "mr-2" }}><FaMoneyBillTrendUp /></IconContext.Provider>} figures={convertToNumber(data[0])} difference={convertToNumber(data[0])} />
+            <DashboardCard text="Total Purchase" icon={<IconContext.Provider value={{ color: "red", size: '24px', className: "mr-2" }}><GrMoney /></IconContext.Provider>} figures={convertToNumber(data[1])} difference={convertToNumber(data[1])} />
+            <DashboardCard text="Profit/Loss" icon={<IconContext.Provider value={{ color: "green", size: '24px', className: "mr-2" }}><GrMoney /></IconContext.Provider>} figures={convertToNumber(data[2])} difference={convertToNumber(data[2])} />
+            <DashboardCard text="Total Shareholders" icon={<IconContext.Provider value={{ color: "blue", size: '24px', className: "mr-2" }}><IoPeople /></IconContext.Provider>} figures={convertToNumber(data[3])} />
+        </div>
+
+    )
+}
+
+export default DashBoardCardGroup
