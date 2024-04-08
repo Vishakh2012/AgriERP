f315cb9281582376dc226551867dd99a89027e3e
ebinjose02 <ebin371@gmail.com>
2024-04-08 21:08:44 +0530

    FEAT:  - Modularized Farmer Forms  - Added new fields in Forms  - Created forms for adding new Staff

---
+++ app/src/page/Farmers/AddNewFarmer/FarmerAddress.tsx
@@ -0,0 +1,112 @@
+import InputComponent from '@/components/Input/Input'
+
+
+const FarmerAddress = ({ formData, handleChange, handleKeyPress }) => {
+   
+  return (
+    
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+        <div>
+        
+        <InputComponent
+          label="Address Line 1"
+          type="text"
+          name="addressLine1"
+          value={formData.addressLine1}
+          onChange={handleChange}
+          placeholder="Address Line 1"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="Address Line 2"
+          type="text"
+          name="addressLine2"
+          value={formData.addressLine2}
+          onChange={handleChange}
+          placeholder="Address Line 2"
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="State"
+          type="text"
+          name="state"
+          value={formData.state}
+          onChange={handleChange}
+          placeholder="State"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="District"
+          type="text"
+          name="district"
+          value={formData.district}
+          onChange={handleChange}
+          placeholder="District"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="City"
+          type="text"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+          placeholder="City"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        
+        <InputComponent
+          label="Block"
+          type="text"
+          name="block"
+          value={formData.block}
+          onChange={handleChange}
+          placeholder="Block"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="Post Office"
+          type="text"
+          name="postOffice"
+          value={formData.postOffice}
+          onChange={handleChange}
+          placeholder="Post Office"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        <div>
+        <InputComponent
+          label="Pincode"
+          type="text"
+          name="pincode"
+          value={formData.pincode}
+          onChange={handleChange}
+          placeholder="Pincode"
+          required
+          onKeyDown={handleKeyPress}
+        />
+        </div>
+        
+        </div>
+    
+  )
+}
+
+export default FarmerAddress
---
+++ app/src/page/Staff_Details/AddNewStaff/StaffForms.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header/Header'
+import StaffFormsCombined from './StaffFormsCombined'
+
+const StaffForms = () => {
+  return (
+    <>
+    <Header text='Add New Staff'/>
+    <StaffFormsCombined/>
+    </>
+  )
+}
+
+export default StaffForms
