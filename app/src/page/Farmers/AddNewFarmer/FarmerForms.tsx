e1b37ebda2c3b2337941a463ffe5105c58e6afee
vishakh prakash <vishakhpro2002@gmail.com>
2024-04-11 17:38:21 +0530

    FEAT: - correction for farmer forms allignment

--- app/src/page/Farmers/AddNewFarmer/FarmerForms.tsx
+++ app/src/page/Farmers/AddNewFarmer/FarmerForms.tsx
@@ -3,10 +3,10 @@ import Header from '@/components/Header/Header'
 
 const FarmerForms = () => {
   return (
-    <>
+    <div className='w-full ml-4 h-screen'>
       <Header text='Add New Farmer'/>
       <Forms/>
-    </>
+    </div>
   )
 }
 
