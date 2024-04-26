caddf84a4a81366aabc4341f00164333db1e7260
sumesh s <sumeshs0202@gmail.com>
2024-04-26 23:07:55 +0530

    FEAT:     -connected api/fpo/get

--- app/src/page/FPO Details/FPO Form/FPO Form components/FpoBasicDetails.tsx
+++ app/src/page/FPO Details/FPO Form/FPO Form components/FpoBasicDetails.tsx
@@ -1,119 +1,110 @@
-import InputComponent from '@/components/Input/Input'
-import { ChangeEvent } from 'react';
+import InputComponent from "@/components/Input/Input";
 
-interface Formdetails{
-  formData:any,
-  handleChange?:any,
-  handleKeyPress?:any,
-  onClick?:any
-  disabled?:boolean
-  onFileChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+interface Formdetails {
+  formData: any;
+  handleChange?: any;
+  handleKeyPress?: any;
+  onClick?: any;
+  disabled?: boolean;
 }
 
-const FpoBasicDetails:React.FC<Formdetails> = ({ formData, handleChange, handleKeyPress ,onClick,disabled,onFileChange }) => {
+const FpoBasicDetails: React.FC<Formdetails> = ({
+  formData,
+  handleChange,
+  handleKeyPress,
+  onClick,
+  disabled,
+}) => {
   return (
     <>
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
-     
-    <div>
-        <InputComponent
-          label="FPO Name"
-          type="text"
-          name="fpoName"
-          value={formData.fpoName}
-          onClick={onClick}
-          onChange={handleChange}
-          placeholder="FPO Name"
-          disabled={disabled}
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
         <div>
-        <InputComponent
-          label="FPO Registration Number"
-          type="text"
-          name="fpoRegNo"
-          value={formData.fpoRegNo}
-          onClick={onClick}
-          onChange={handleChange}
-          disabled={disabled}
-          placeholder="FPO Registration Number"
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
-        <div>
-        <InputComponent
-          label="CEO"
-          type="text"
-          name="fpoCeo"
-          value={formData.fpoCeo}
-          onClick={onClick}
-          onChange={handleChange}
-          disabled={disabled}
-          placeholder="CEO"
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
-        <div>
-        <InputComponent
-          label="Date of Formation"
-          type="date"
-          name="dateOfFormation"
-          value={formData.dateOfFormation}
-          onClick={onClick}
-          onChange={handleChange}
-          disabled={disabled}
-          placeholder='Date of Formation'
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
-        <div>
-        <InputComponent
-          label="Email"
-          type="email"
-          name="email"
-          value={formData.email}
-          onClick={onClick}
-          disabled={disabled}
-          onChange={handleChange}
-          placeholder='Email'
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
-        <div>
-        <InputComponent
-          label="Phone Number"
-          type="number"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onClick={onClick}
-          disabled={disabled}
-          onChange={handleChange}
-          placeholder='Phone Number'
-          required
-          onKeyDown={handleKeyPress}
-        />
-        </div>
-      </div>
-      <div>
-      <InputComponent
-            label="Upload Image"
-            type="file"
-            accept="image/*" // Accept only image files
-            name="headerImage"
-            onChange={onFileChange} // Call onFileChange when file is selected
+          <InputComponent
+            label="FPO Name"
+            type="text"
+            name="fpoName"
+            value={formData.name}
+            onClick={onClick}
+            onChange={handleChange}
+            placeholder="FPO Name"
             disabled={disabled}
             required
+            onKeyDown={handleKeyPress}
           />
+        </div>
+        <div>
+          <InputComponent
+            label="FPO Registration Number"
+            type="text"
+            name="fpoRegNo"
+            value={formData.fpoRegNo}
+            onClick={onClick}
+            onChange={handleChange}
+            disabled={disabled}
+            placeholder="FPO Registration Number"
+            required
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+        <div>
+          <InputComponent
+            label="CEO"
+            type="text"
+            name="fpoCeo"
+            value={formData.fpoCeo}
+            onClick={onClick}
+            onChange={handleChange}
+            disabled={disabled}
+            placeholder="CEO"
+            required
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+        <div>
+          <InputComponent
+            label="Date of Formation"
+            type="date"
+            name="dateOfFormation"
+            value={formData.dateOfFormation}
+            onClick={onClick}
+            onChange={handleChange}
+            disabled={disabled}
+            placeholder="Date of Formation"
+            required
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+        <div>
+          <InputComponent
+            label="Email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onClick={onClick}
+            disabled={disabled}
+            onChange={handleChange}
+            placeholder="Email"
+            required
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+        <div>
+          <InputComponent
+            label="Phone Number"
+            type="number"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onClick={onClick}
+            disabled={disabled}
+            onChange={handleChange}
+            placeholder="Phone Number"
+            required
+            onKeyDown={handleKeyPress}
+          />
+        </div>
       </div>
-      
-        </>
-  )
-}
+    </>
+  );
+};
 
-export default FpoBasicDetails
+export default FpoBasicDetails;
